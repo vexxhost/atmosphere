8f469a882634646d838b4a190a123d5944e3ddb8
Mohammed Naser <mnaser@vexxhost.com>
2022-03-14 18:17:42 -0400

    Added Zuul artifacts for builds
    Change-Id: I586ffaed81e516f8f1806044f9191533404a5b3c

---
+++ tools/generate-galaxy-yml.py
@@ -0,0 +1,28 @@
+import yaml
+from pbr.version import VersionInfo
+
+GALAXY_YML = {
+    'namespace': 'vexxhost',
+    'name': 'atmosphere',
+    'version': VersionInfo('ansible-collection-atmosphere').release_string(),
+    'readme': 'README.md',
+    'authors': [
+        "Mohammed Naser <mnaser@vexxhost.com>",
+    ],
+    'dependencies': {
+        'ansible.posix': '1.3.0',
+        'ansible.utils': '2.5.2',
+        'community.crypto': '2.2.3',
+        'community.general': '4.5.0',
+        'kubernetes.core': '2.2.3',
+        'openstack.cloud': '1.7.0',
+    },
+    'build_ignore': [
+        '.tox',
+        '.vscode',
+        'doc',
+    ],
+}
+
+with open('galaxy.yml', 'w') as f:
+    yaml.dump(GALAXY_YML, f, default_flow_style=False)
