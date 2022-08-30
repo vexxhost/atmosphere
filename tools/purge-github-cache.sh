239772efa508e766014a2bc663551dff2e9ac57c
Mohammed Naser <mnaser@vexxhost.com>
2022-08-29 23:42:54 -0400

    Fix cache

--- tools/purge-github-cache.sh
+++ tools/purge-github-cache.sh
@@ -4,5 +4,5 @@ gh api \
   --header "Accept: application/vnd.github+json" \
   --jq '.actions_caches[].id' \
   --paginate \
-  /repos/vexxhost/atmosphere-images/actions/caches |
-    xargs -P10 -I {} /bin/bash -xec "gh api --method DELETE /repos/vexxhost/atmosphere-images/actions/caches/{} 2>&1 >/dev/null"
+  /repos/vexxhost/atmosphere/actions/caches |
+    xargs -P10 -I {} /bin/bash -xec "gh api --method DELETE /repos/vexxhost/atmosphere/actions/caches/{} 2>&1 >/dev/null"
