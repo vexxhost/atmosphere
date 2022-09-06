ce75b413c117ecda2fe02e53ef2545b967277910
Mohammed Naser <mnaser@vexxhost.com>
2022-09-06 15:39:42 -0400

    build: added `--admin` flag

--- cmd/atmosphere-ci/image_repo_sync.go
+++ cmd/atmosphere-ci/image_repo_sync.go
@@ -9,6 +9,8 @@ import (
 )
 
 var (
+	admin bool
+
 	imageRepoSyncCmd = &cobra.Command{
 		Use:   "sync [project]",
 		Short: "Sync image repository",
@@ -18,6 +20,11 @@ var (
 			ctx := context.TODO()
 
 			repo := image_repositories.NewImageRepository(args[0])
+
+			if admin {
+				repo.UpdateGithubConfiguration(ctx)
+			}
+
 			err := repo.Synchronize(ctx)
 			if err != nil {
 				log.Panic(err)
@@ -27,5 +34,7 @@ var (
 )
 
 func init() {
+	imageRepoCmd.PersistentFlags().BoolVar(&admin, "admin", false, "Run using admin PAT (will update repo configs)")
+
 	imageRepoCmd.AddCommand(imageRepoSyncCmd)
 }
