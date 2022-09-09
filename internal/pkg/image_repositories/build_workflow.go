481bfe00d6469b65fa6215395e79b94aa3cff58c
Mohammed Naser <mnaser@vexxhost.com>
2022-09-08 21:11:36 -0400

    build(glance): add missing packages

--- internal/pkg/image_repositories/build_workflow.go
+++ internal/pkg/image_repositories/build_workflow.go
@@ -20,7 +20,7 @@ var PROFILES map[string]string = map[string]string{
 }
 var DIST_PACAKGES map[string]string = map[string]string{
 	"designate":     "bind9utils",
-	"glance":        "kubectl",
+	"glance":        "kubectl nvme-cli util-linux",
 	"heat":          "curl",
 	"ironic":        "ethtool lshw iproute2",
 	"monasca-agent": "iproute2 libvirt-clients lshw",
