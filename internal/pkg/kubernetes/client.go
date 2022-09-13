884a145cc328e3d62ea00882e336b8249b39ce8b
Mohammed Naser <mnaser@vexxhost.com>
2022-09-13 16:09:15 -0400

    chore: clean-up old clientset

--- internal/pkg/kubernetes/client.go
+++ internal/pkg/kubernetes/client.go
@@ -4,10 +4,7 @@ import (
 	helmv2 "github.com/fluxcd/helm-controller/api/v2beta1"
 	sourcev1 "github.com/fluxcd/source-controller/api/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/scheme"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/tools/clientcmd"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -20,20 +17,4 @@ func GetClient() (client.Client, error) {
 	_ = helmv2.AddToScheme(scheme)
 
 	return client.New(ctrl.GetConfigOrDie(), client.Options{Scheme: scheme})
-}
-
-func GetClientSet() (*kubernetes.Clientset, error) {
-	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
-	configOverrides := &clientcmd.ConfigOverrides{}
-
-	kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
-
-	config, err := kubeConfig.ClientConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	_ = helmv2.AddToScheme(scheme.Scheme)
-
-	return kubernetes.NewForConfig(config)
 }
