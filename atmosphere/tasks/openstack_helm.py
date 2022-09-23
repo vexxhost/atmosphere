1a6df9192e9cd6a2df180546d62d5c065003926d
Mohammed Naser <mnaser@vexxhost.com>
2022-09-23 12:01:21 -0400

    fix: move memcached to operator

--- atmosphere/tasks/openstack_helm.py
+++ atmosphere/tasks/openstack_helm.py
@@ -1,5 +1,5 @@
 from atmosphere.models.openstack_helm import values
-from atmosphere.tasks import kubernetes
+from atmosphere.tasks import flux, kubernetes
 
 
 class CreateOrUpdateReleaseSecretTask(kubernetes.CreateOrUpdateSecretTask):
@@ -10,4 +10,28 @@ class CreateOrUpdateReleaseSecretTask(kubernetes.CreateOrUpdateSecretTask):
             values.Values.for_chart(chart).secret_data,
             *args,
             **kwargs,
+        )
+
+
+class CreateOrUpdateHelmReleaseTask(flux.CreateOrUpdateHelmReleaseTask):
+    def __init__(
+        self,
+        namespace: str,
+        name: str,
+        repository: str,
+        version: str,
+    ):
+        super().__init__(
+            namespace=namespace,
+            name=name,
+            repository=repository,
+            chart=name,
+            version=version,
+            values_from=[
+                {
+                    "kind": "Secret",
+                    "name": f"atmosphere-{name}",
+                }
+            ],
+            requires=set([f"secret-{namespace}-atmosphere-{name}"]),
         )
