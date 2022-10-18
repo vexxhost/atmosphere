781ab1de39fbab14977773b7bdacb209668f1aad
Mohammed Naser <mnaser@vexxhost.com>
2022-10-18 02:55:25 +0000

    feat: add object storage

--- atmosphere/tasks/kubernetes/rook.py
+++ atmosphere/tasks/kubernetes/rook.py
@@ -1,3 +1,5 @@
+import textwrap
+
 import mergedeep
 import pykube
 
@@ -20,8 +22,7 @@ class ApplyCephClusterTask(base.ApplyKubernetesObjectTask):
             name=name,
             requires=set(
                 [
-                    "namespace",
-                    f"helm-release-{constants.HELM_REPOSITORY_ROOK_CEPH}-{constants.HELM_RELEASE_ROOK_CEPH_NAME}",
+                    f"helm-release-{namespace}-{constants.HELM_RELEASE_ROOK_CEPH_NAME}",
                 ]
             ),
         )
@@ -43,6 +44,75 @@ class ApplyCephClusterTask(base.ApplyKubernetesObjectTask):
                     },
                     "external": {
                         "enable": True,
+                    },
+                },
+            },
+        )
+
+
+class CephObjectStore(pykube.objects.NamespacedAPIObject):
+    version = "ceph.rook.io/v1"
+    endpoint = "cephobjectstores"
+    kind = "CephObjectStore"
+
+
+class ApplyCephObjectStoreTask(base.ApplyKubernetesObjectTask):
+    def __init__(self, namespace: str, cluster: str, name: str):
+        super().__init__(
+            kind=CephObjectStore,
+            namespace=namespace,
+            name=name,
+            requires=set(
+                [
+                    f"ceph-cluster-{namespace}-{cluster}",
+                ]
+            ),
+        )
+
+    def generate_object(self) -> CephObjectStore:
+        return CephObjectStore(
+            self.api,
+            {
+                "apiVersion": self._obj_kind.version,
+                "kind": self._obj_kind.kind,
+                "metadata": {
+                    "name": self._obj_name,
+                    "namespace": self._obj_namespace,
+                },
+                "spec": {
+                    "metadataPool": {
+                        "failureDomain": "host",
+                        "replicated": {
+                            "size": 3,
+                        },
+                    },
+                    "dataPool": {
+                        "failureDomain": "host",
+                        "replicated": {
+                            "size": 3,
+                        },
+                    },
+                    "preservePoolsOnDelete": True,
+                    "gateway": {
+                        "port": 80,
+                        "instances": 3,
+                        "placement": {
+                            "nodeAffinity": {
+                                "requiredDuringSchedulingIgnoredDuringExecution": {
+                                    "nodeSelectorTerms": [
+                                        {
+                                            "matchExpressions": [
+                                                {
+                                                    "key": "openstack-control-plane",
+                                                    "operator": "In",
+                                                    "values": ["enabled"],
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                },
+                            },
+                        },
                     },
                 },
             },
@@ -93,5 +163,34 @@ def tasks_from_config(config: config.RookCephChartConfig) -> list:
                 "maxMonId": "2",
             },
         ),
+        v1.ApplyConfigMapTask(
+            namespace=constants.NAMESPACE_ROOK_CEPH,
+            name="rook-config-override",
+            data={
+                "config": textwrap.dedent(
+                    """\
+                    [client]
+                    rgw keystone api version = 3
+                    rgw keystone url =  http://keystone-api.openstack.svc.cluster.local:5000
+                    rgw keystone admin user = admin
+                    rgw keystone admin password = FOOBAR
+                    rgw_keystone admin domain = services
+                    rgw_keystone admin project = admin
+                    rgw keystone implicit tenants = true
+                    rgw keystone accepted roles = member,admin
+                    rgw_keystone accepted admin roles = admin
+                    rgw keystone token cache size = 0
+                    rgw s3 auth use keystone = true
+                    rgw swift account in url = true
+                    rgw swift versioning enabled = true
+                """
+                ),
+            },
+        ),
         ApplyCephClusterTask(namespace="rook-ceph", name="rook-ceph"),
+        ApplyCephObjectStoreTask(
+            namespace="rook-ceph",
+            cluster="rook-ceph",
+            name="rook-ceph",
+        ),
     ]
