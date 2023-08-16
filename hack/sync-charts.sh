a73bc0ae87ad664575f744bc3af3a7560f599c15
ricolin <rlin@vexxhost.com>
2023-08-16 14:29:32 +0800

    bump Octavia to 0.2.9

--- hack/sync-charts.sh
+++ hack/sync-charts.sh
@@ -199,15 +199,9 @@ curl 'https://review.opendev.org/changes/openstack%2Fopenstack-helm~883168/revis
   | filterdiff -p1 -i 'heat/*' \
   | patch -p2 -d ${ATMOSPHERE}/charts/heat
 
-OCTAVIA_VERSION=0.2.8
+OCTAVIA_VERSION=0.2.9
 curl -sL https://tarballs.opendev.org/openstack/openstack-helm/octavia-${OCTAVIA_VERSION}.tgz \
   | tar -xz -C ${ATMOSPHERE}/charts
-curl 'https://review.opendev.org/changes/openstack%2Fopenstack-helm~891029/revisions/1/patch?download' \
-  | base64 --decode \
-  | filterdiff -p1 -x 'releasenotes/*' \
-  | filterdiff -p2 -x 'Chart.yaml' \
-  | filterdiff -p1 -i 'octavia/*' \
-  | patch -p2 -d ${ATMOSPHERE}/charts/octavia
 
 MAGNUM_VERSION=0.2.9
 curl -sL https://tarballs.opendev.org/openstack/openstack-helm/magnum-${MAGNUM_VERSION}.tgz \
