0a53f164595ea091694ab6e5863267b2530130ec
okozachenko1203 <okozachenko1203@users.noreply.github.com>
2023-08-19 00:01:35 +1000

    feat: Rollout horizon deployment whenever logo changed

--- hack/sync-charts.sh
+++ hack/sync-charts.sh
@@ -170,9 +170,15 @@ MAGNUM_VERSION=0.2.9
 curl -sL https://tarballs.opendev.org/openstack/openstack-helm/magnum-${MAGNUM_VERSION}.tgz \
   | tar -xz -C ${ATMOSPHERE}/charts
 
-HORIZON_VERSION=0.3.8
+HORIZON_VERSION=0.3.10
 curl -sL https://tarballs.opendev.org/openstack/openstack-helm/horizon-${HORIZON_VERSION}.tgz \
   | tar -xz -C ${ATMOSPHERE}/charts
+curl 'https://review.opendev.org/changes/openstack%2Fopenstack-helm~891987/revisions/1/patch?download' \
+  | base64 --decode \
+  | filterdiff -p1 -x 'releasenotes/*' \
+  | filterdiff -p2 -x 'Chart.yaml' \
+  | filterdiff -p1 -i 'horizon/*' \
+  | patch -p2 -d ${ATMOSPHERE}/charts/horizon
 
 TEMPEST_VERSION=0.2.7
 curl -sL https://tarballs.opendev.org/openstack/openstack-helm/tempest-${TEMPEST_VERSION}.tgz \
