a7a27f8f3e4ee811efdf6978fc69cb485adc3d4c
Mohammed Naser <mnaser@vexxhost.com>
2023-10-09 22:47:31 -0400

    fix(libvirt): add HOSTNAME_FQDN to certificate (#627)

--- hack/sync-charts.sh
+++ hack/sync-charts.sh
@@ -109,7 +109,7 @@ curl -sL https://tarballs.opendev.org/openstack/openstack-helm-infra/openvswitch
 LIBVIRT_VERSION=0.1.23
 curl -sL https://tarballs.opendev.org/openstack/openstack-helm-infra/libvirt-${LIBVIRT_VERSION}.tgz \
   | tar -xz -C ${ATMOSPHERE}/charts
-curl 'https://review.opendev.org/changes/openstack%2Fopenstack-helm-infra~893406/revisions/5/patch?download' \
+curl 'https://review.opendev.org/changes/openstack%2Fopenstack-helm-infra~893406/revisions/6/patch?download' \
   | base64 --decode \
   | filterdiff -p1 -x 'releasenotes/*' \
   | filterdiff -p2 -x 'Chart.yaml' \
