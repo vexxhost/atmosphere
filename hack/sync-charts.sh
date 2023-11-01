75b68748cec8e5bc729f38d20b5ddd433b97278e
ricolin <rlin@vexxhost.com>
2023-11-02 01:46:24 +0800

    chore(libvirt): bump libvirt charts

--- hack/sync-charts.sh
+++ hack/sync-charts.sh
@@ -114,10 +114,10 @@ OPEN_VSWITCH_VERSION=0.1.15
 curl -sL https://tarballs.opendev.org/openstack/openstack-helm-infra/openvswitch-${OPEN_VSWITCH_VERSION}.tgz \
   | tar -xz -C ${ATMOSPHERE}/charts
 
-LIBVIRT_VERSION=0.1.23
+LIBVIRT_VERSION=0.1.27
 curl -sL https://tarballs.opendev.org/openstack/openstack-helm-infra/libvirt-${LIBVIRT_VERSION}.tgz \
   | tar -xz -C ${ATMOSPHERE}/charts
-curl 'https://review.opendev.org/changes/openstack%2Fopenstack-helm-infra~893406/revisions/6/patch?download' \
+curl 'https://review.opendev.org/changes/openstack%2Fopenstack-helm-infra~893406/revisions/9/patch?download' \
   | base64 --decode \
   | filterdiff -p1 -x 'releasenotes/*' \
   | filterdiff -p2 -x 'Chart.yaml' \
