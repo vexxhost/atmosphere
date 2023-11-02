d5988ea44f9ced83e079a2edf0e634426dac4609
ricolin <rlin@vexxhost.com>
2023-11-02 17:24:24 +0800

    fix(sync-charts): Remove extra files

--- hack/sync-charts.sh
+++ hack/sync-charts.sh
@@ -85,6 +85,8 @@ curl 'https://review.opendev.org/changes/openstack%2Fopenstack-helm~899867/revis
   | filterdiff -p2 -x 'Chart.yaml' \
   | filterdiff -p1 -i 'keystone/*' \
   | patch -p2 -d ${ATMOSPHERE}/charts/keystone
+# Remove extra files before 899867 merged
+rm ${ATMOSPHERE}/charts/keystone/templates/bin/_domain-manage-init.sh.tpl ${ATMOSPHERE}/charts/keystone/templates/bin/_domain-manage.py.tpl
 
 BARBICAN_VERSION=0.3.6
 curl -sL https://tarballs.opendev.org/openstack/openstack-helm/barbican-${BARBICAN_VERSION}.tgz \
@@ -123,6 +125,8 @@ curl 'https://review.opendev.org/changes/openstack%2Fopenstack-helm~899914/revis
   | filterdiff -p2 -x 'Chart.yaml' \
   | filterdiff -p1 -i 'placement/*' \
   | patch -p2 -d ${ATMOSPHERE}/charts/placement
+# Remove extra files before 899914 merged
+rm -r ${ATMOSPHERE}/charts/placement/values_overrides/
 
 OPEN_VSWITCH_VERSION=0.1.15
 curl -sL https://tarballs.opendev.org/openstack/openstack-helm-infra/openvswitch-${OPEN_VSWITCH_VERSION}.tgz \
