8671cf83f3a7570a819643f52655e9d17aa760b0
ricolin <rlin@vexxhost.com>
2023-11-02 14:53:41 +0800

    chore(placement): bump placement charts

--- hack/sync-charts.sh
+++ hack/sync-charts.sh
@@ -118,9 +118,15 @@ curl 'https://review.opendev.org/changes/openstack%2Fopenstack-helm~899814/revis
   | filterdiff -p1 -i 'cinder/*' \
   | patch -p2 -d ${ATMOSPHERE}/charts/cinder
 
-PLACEMENT_VERSION=0.3.7
+PLACEMENT_VERSION=0.3.9
 curl -sL https://tarballs.opendev.org/openstack/openstack-helm/placement-${PLACEMENT_VERSION}.tgz \
   | tar -xz -C ${ATMOSPHERE}/charts
+curl 'https://review.opendev.org/changes/openstack%2Fopenstack-helm~899914/revisions/3/patch?download' \
+  | base64 --decode \
+  | filterdiff -p1 -x 'releasenotes/*' \
+  | filterdiff -p2 -x 'Chart.yaml' \
+  | filterdiff -p1 -i 'placement/*' \
+  | patch -p2 -d ${ATMOSPHERE}/charts/placement
 
 OPEN_VSWITCH_VERSION=0.1.15
 curl -sL https://tarballs.opendev.org/openstack/openstack-helm-infra/openvswitch-${OPEN_VSWITCH_VERSION}.tgz \
