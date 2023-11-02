6d747528c581c2ecdd7134a1f96470ed46674190
ricolin <rlin@vexxhost.com>
2023-11-02 14:15:48 +0800

    chore(senlin): bump senlin charts

--- hack/sync-charts.sh
+++ hack/sync-charts.sh
@@ -185,6 +185,12 @@ curl 'https://review.opendev.org/changes/openstack%2Fopenstack-helm~899809/revis
 SENLIN_VERSION=0.2.9
 curl -sL https://tarballs.opendev.org/openstack/openstack-helm/senlin-${SENLIN_VERSION}.tgz \
   | tar -xz -C ${ATMOSPHERE}/charts
+curl 'https://review.opendev.org/changes/openstack%2Fopenstack-helm~899913/revisions/1/patch?download' \
+  | base64 --decode \
+  | filterdiff -p1 -x 'releasenotes/*' \
+  | filterdiff -p2 -x 'Chart.yaml' \
+  | filterdiff -p1 -i 'senlin/*' \
+  | patch -p2 -d ${ATMOSPHERE}/charts/senlin
 
 DESIGNATE_VERSION=0.2.9
 curl -sL https://tarballs.opendev.org/openstack/openstack-helm/designate-${DESIGNATE_VERSION}.tgz \
