3ad0af15576aa0766a08c3593100f77851b04a56
Mohammed Naser <mnaser@vexxhost.com>
2023-11-27 10:11:15 -0500

    chore(images): add pykmip to barbican

--- internal/pkg/image_repositories/build_workflow.go
+++ internal/pkg/image_repositories/build_workflow.go
@@ -39,6 +39,7 @@ var DIST_PACAKGES map[string]string = map[string]string{
 	"nova":          "ovmf qemu-efi-aarch64 lsscsi nvme-cli sysfsutils udev util-linux ndctl",
 }
 var PIP_PACKAGES map[string][]string = map[string][]string{
+	"barbican":      {"pykmip"},
 	"cinder":        {"purestorage"},
 	"glance":        {"glance_store[cinder]"},
 	"horizon":       {"git+https://github.com/openstack/designate-dashboard.git@stable/${{ matrix.release }}", "git+https://github.com/openstack/heat-dashboard.git@stable/${{ matrix.release }}", "git+https://github.com/openstack/ironic-ui.git@stable/${{ matrix.release }}", "git+https://github.com/vexxhost/magnum-ui.git@stable/${{ matrix.release }} git+https://github.com/openstack/neutron-vpnaas-dashboard.git@stable/${{ matrix.release }} git+https://github.com/openstack/octavia-dashboard.git@stable/${{ matrix.release }} git+https://github.com/openstack/senlin-dashboard.git@stable/${{ matrix.release }}", "git+https://github.com/openstack/monasca-ui.git@stable/${{ matrix.release }}", "git+https://github.com/openstack/manila-ui.git@stable/${{ matrix.release }}"},
