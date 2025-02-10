e5c97e2ebda1d4f0031684b4330f76b85c058463
Mohammed Naser <mnaser@vexxhost.com>
2025-02-10 10:52:10 -0500

    Add unit tests to for {runtime,priority}ClassName
    These unit tests go over all the possible pods using the
    mount feature and make sure they are all listed in the
    runtime and priority class, which will make sure we don't
    miss any.
    
    Change-Id: Iba2e24bebd70f0d99f44566a1b0f115e4faa8f5b

--- internal/testutils/oslo_db.go
+++ internal/testutils/oslo_db.go
@@ -1,6 +1,7 @@
 package testutils
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,4 +13,33 @@ func TestDatabaseConf(t *testing.T, config *openstack_helm.DatabaseConf) {
 	assert.Equal(t, 600, config.ConnectionRecycleTime)
 	assert.Equal(t, 5, config.MaxPoolSize)
 	assert.Equal(t, -1, config.MaxRetries)
+}
+
+func podNameForClass(pod string) string {
+	// There are a few pods which are built/created inside "helm-toolkit" so
+	// we cannot refer to them by their full name or the code will get real
+	// messy.
+	if strings.HasSuffix(pod, "db_init") {
+		return "db_init"
+	} else if strings.HasSuffix(pod, "db_sync") {
+		return "db_sync"
+	} else if strings.HasSuffix(pod, "_bootstrap") {
+		return "bootstrap"
+	}
+
+	return pod
+}
+
+func TestAllPodsHaveRuntimeClass(t *testing.T, vals *openstack_helm.HelmValues) {
+	for pod := range vals.Pod.Mounts {
+		podName := podNameForClass(pod)
+		assert.Contains(t, vals.Pod.RuntimeClass, podName)
+	}
+}
+
+func TestAllPodsHavePriorityClass(t *testing.T, vals *openstack_helm.HelmValues) {
+	for pod := range vals.Pod.Mounts {
+		podName := podNameForClass(pod)
+		assert.Contains(t, vals.Pod.PriorityClass, podName)
+	}
 }
--- roles/nova/vars_test.go
+++ roles/nova/vars_test.go
@@ -36,4 +36,6 @@ func TestHelmValues(t *testing.T) {
 	require.NoError(t, err)
 
 	testutils.TestDatabaseConf(t, vals.Conf.Nova.Database)
+	testutils.TestAllPodsHaveRuntimeClass(t, vals)
+	testutils.TestAllPodsHavePriorityClass(t, vals)
 }
