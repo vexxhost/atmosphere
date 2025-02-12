a31cb77ec455ee7ac056efa56f8e7e8c9142afbc
Jason Hall <jason.hall@runlevel6.us>
2025-02-12 10:33:39 -0600

    Feature: pod affinity changes for OVN
    Deploy OVN northd using the same pod affinity rules
    used with the NB and SB statefulsets. Also change
    default anti-affinity for NB and SB statefulsets
    to requiredDuringSchedulingIgnoredDuringExecution
    over preferredDuringSchedulingIgnoredDuringExecution
    
    These unit tests go over all the statefulset and
    deployment  pods using the and make sure they are
    all using the right anti-affinity rules.
    
    Change-Id: I2c8b5bf7515040e715bdeed4410acf6656578133

--- internal/testutils/oslo_db.go
+++ internal/testutils/oslo_db.go
@@ -1,9 +1,11 @@
 package testutils
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/vexxhost/atmosphere/internal/openstack_helm"
 )
@@ -12,4 +14,35 @@ func TestDatabaseConf(t *testing.T, config *openstack_helm.DatabaseConf) {
 	assert.Equal(t, 600, config.ConnectionRecycleTime)
 	assert.Equal(t, 5, config.MaxPoolSize)
 	assert.Equal(t, -1, config.MaxRetries)
+}
+
+func podNameForClass(pod string) string {
+	// There are a few pods which are built/created inside "helm-toolkit" so
+	// we cannot refer to them by their full name or the code will get real
+	// messy.
+	if strings.HasSuffix(pod, "db_init") {
+		return "db_init"
+	} else if strings.HasSuffix(pod, "db_sync") {
+		return "db_sync"
+	} else if strings.HasSuffix(pod, "_bootstrap") {
+		return "bootstrap"
+	}
+	return pod
+}
+
+func TestAllPodsHaveAntiAffinityType(t *testing.T, vals *openstack_helm.HelmValues) {
+	for pod := range vals.Pod.AntiAffinityType {
+		podName := podNameForClass(pod)
+
+		expected := "requiredDuringSchedulingIgnoredDuringExecution"
+
+		defaultRaw, ok := vals.Pod.AntiAffinityType["default"]
+		require.True(t, ok, "default key not found in affinity.anti.type block")
+
+		actual, ok := defaultRaw.(string)
+		require.True(t, ok, "default anti affinity type is not a string")
+
+		assert.Equal(t, expected, actual, "anti affinity type does not match expected value")
+		assert.Contains(t, vals.Pod.AntiAffinityType, podName)
+	}
 }
