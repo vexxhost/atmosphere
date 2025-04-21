c4808cbc3f29095bf459e3fae8eeff64f8cd783d
Mohammed Naser <mnaser@vexxhost.com>
2025-04-21 16:45:30 +0000

    Revert "Add new release script"
    This reverts commit 446f01278419776735838ec244597d2b1112cda9.

--- build/release.sh
+++ build/release.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -xe
 
 VERSION=$1
+LATEST_VERSION=$2
 
-if [ -z "$VERSION" ]; then
+if [ -z "$VERSION" ] || [ -z "$LATEST_VERSION" ]; then
   echo "Usage: $0 <version> <latest_version>"
   exit 1
 fi
@@ -15,9 +16,8 @@ sed -i s/^atmosphere_version:.*/atmosphere_version:\ $VERSION/ roles/defaults/de
 git add galaxy.yml roles/defaults/defaults/main.yml
 git commit -m "Release $VERSION"
 
-# Create a local tag
-git tag v$VERSION
-
 # Push the release commit
-git push gerrit refs/heads/$(git rev-parse --abbrev-ref HEAD)
-git push --tags gerrit refs/heads/$(git rev-parse --abbrev-ref HEAD)
+git push
+
+# Create GitHub release with gh CLI
+gh release create v$VERSION --target $(git rev-parse HEAD) --generate-notes --latest=$LATEST_VERSION
