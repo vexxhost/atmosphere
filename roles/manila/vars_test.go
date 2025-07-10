ade82ed0d296e46406a3d4f7e8b7a083e991e3e5
ricolin <rlin@vexxhost.com>
2025-07-10 15:26:06 +0800

    [helm-toolkit][manila] fix ClassName
    Change-Id: Ibf1e8f015605dd8debbb07206aa63f865ed978bb
    Signed-off-by: ricolin <rlin@vexxhost.com>

--- roles/manila/vars_test.go
+++ roles/manila/vars_test.go
@@ -34,6 +34,30 @@ func TestMain(m *testing.M) {
 func TestHelmValues(t *testing.T) {
 	vals, err := openstack_helm.CoalescedHelmValues("../../charts/manila", &vars.HelmValues)
 	require.NoError(t, err)
+	// (rlin): Before you add any new priority class here.
+	// Make sure we do use snippets tool
+	// helm-toolkit.snippets.kubernetes_pod_priority_class
+	// for the actual template. Like:
+	// {{ tuple "heat_api" . | include "helm-toolkit.snippets.kubernetes_pod_priority_class" }}
+	vars.HelmValues.Pod.PriorityClass = map[string]string{
+		"manila_api": "high-priority",
+		"manila_data": "high-priority",
+		"manila_scheduler": "high-priority",
+		"manila_share": "high-priority",
+		"manila_tests": "high-priority",
+	}
+	// (rlin): Before you add any new runtime class here.
+	// Make sure we do use snippets tool
+	// helm-toolkit.snippets.kubernetes_pod_runtime_class
+	// for the actual template. Like:
+	// {{ tuple "heat_api" . | include "helm-toolkit.snippets.kubernetes_pod_runtime_class" }}
+	vars.HelmValues.Pod.RuntimeClass = map[string]string{
+		"manila_api": "kata-clh",
+		"manila_data": "kata-clh",
+		"manila_scheduler": "kata-clh",
+		"manila_share": "kata-clh",
+		"manila_tests": "kata-clh",
+	}
 
 	testutils.TestDatabaseConf(t, vals.Conf.Manila.Database)
 	testutils.TestAllPodsHaveRuntimeClass(t, vals)
